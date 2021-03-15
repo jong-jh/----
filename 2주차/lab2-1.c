635e1a7d9fd7a1012120217216f59290e456ad1b
jong_jh <rhfksl0115@gmail.com>
2021-03-15 22:26:39 +0900

    pratice

--- 2주차/lab2-1.c
+++ 2주차/lab2-1.c
@@ -23,7 +23,7 @@ int main(){
     printf("Size of int* : %ld byte\n",sizeof(int*));//64bit 운영체제의 pc를 사용중이라 8byte가 나올 줄 알았다.
     printf("Size of float* : %ld byte\n",sizeof(float*));//예상과는 다르게 4byte가 나와 알아본 결과 32bit로 컴파일을 하면 4byte가 나온다고 하였다.
     printf("Size of double* : %ld byte\n",sizeof(double*));
-    printf("==============================================\n");
+    printf("===============================================\n");
 
     return 0;
 }
